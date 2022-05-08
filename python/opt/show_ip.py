36213c1d723510ad9913df257a4ae65b83c736b1
ymd65536 <ymd65536@gmail.com>
2022-05-08 19:34:10 +0900

    インデント変更

--- python/opt/show_ip.py
+++ python/opt/show_ip.py
@@ -4,7 +4,7 @@ from ipaddress import ip_address
 import socket
 
 if __name__ == "__main__":
-	host_name = socket.gethostname()
-	ip_addr = socket.gethostbyname(host_name)
-	print(host_name)
-	print(ip_addr)
+  host_name = socket.gethostname()
+  ip_addr = socket.gethostbyname(host_name)
+  print(host_name)
+  print(ip_addr)
